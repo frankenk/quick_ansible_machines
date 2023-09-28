d982bd58e63f4bc156d370ef3604a01fcb3a3c44
frankenk <franken@DESKTOP-GHS1O23>
2023-09-28 22:46:15 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # quick_ansible_machines
+
+Just a simple Terraform code to quickly setup instances for Ansible testing. 
---
+++ main.tf
@@ -0,0 +1,134 @@
+data "aws_ami" "aws_linux2" { #Will always get you latest version of AMI
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2", ]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_security_group" "selected" {
+  vpc_id = data.aws_vpc.default.id
+
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_instance" "ansible_nodes" {
+  count                  = 2
+  ami                    = data.aws_ami.aws_linux2.id
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_traffic.id]
+  user_data              = file("scripts/setup.sh")
+
+  tags = {
+    Name = "Ansible node-${count.index + 1}"
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "allow_ssm" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:DescribeAssociation",
+          "ssm:GetDeployablePatchSnapshotForInstance",
+          "ssm:GetDocument",
+          "ssm:DescribeDocument",
+          "ssm:GetManifest",
+          "ssm:ListAssociations",
+          "ssm:ListInstanceAssociations",
+          "ssm:UpdateAssociationStatus",
+          "ssm:UpdateInstanceAssociationStatus",
+          "ssm:UpdateInstanceInformation"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel",
+          "ec2messages:GetEndpoint",
+          "ec2messages:GetMessages",
+          "ec2messages:AcknowledgeMessage"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid      = "IamPassRole"
+        Effect   = "Allow"
+        Action   = "iam:PassRole"
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "iam:PassedToService" = "ec2.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.allow_ssm.arn
+}
+
+resource "aws_security_group" "allow_ssh_traffic" {
+  name        = "tf-allow-ssh"
+  description = "Allow SSH inbound, and all outbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
